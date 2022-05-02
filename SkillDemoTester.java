251a6b53012fa4223da2e3fbe26d306fada8d813
emaresmoreno <emaresmoreno@ucsd.edu>
2022-05-01 21:24:56 -0700

    Corrected error

--- SkillDemoTester.java
+++ SkillDemoTester.java
@@ -4,7 +4,8 @@ import org.junit.*;
 public class SkillDemoTester {
     @Test
     public void test(){
-        assertEquals("checks that 4 is correctly divided by 2", 2, SkillDemo.divide(4,2));
+        assertEquals("checks that 4 is correctly divided by 2", 2,
+         SkillDemo.divide(4,2));
 
     }
     
