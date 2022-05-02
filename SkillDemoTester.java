9d5e84adaa827d0187dae75d5868c2badc6ae56b
emaresmoreno <emaresmoreno@ucsd.edu>
2022-05-01 21:19:49 -0700

    created tester file and an erorr

---
+++ SkillDemoTester.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTester {
+    @Test
+    public void test(){
+        assertEquals("checks that 4 is correctly divided by 2", 2, SkillDemo.divide(4,2));
+
+    }
+    
+}
